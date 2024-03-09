03075cadc7114cc8e269bb738d5812734ef213e8
TheKiet <kietthetran123@gmail.com>
2024-03-09 09:47:22 +0700

    update

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -8,19 +8,19 @@ const Navbar = () => {
             <h1 className='flex-1 text-[#054C73] font-bold text-[34px]'>Furniture</h1>
             <ul className='flex flex-1 justify-between text-[#333333] text-[14px] font-semibold'>
                 <li>
-                    <a href="#">Home</a>
+                    <a href="/">Home</a>
                 </li>
                 <li>
-                    <a href="#">Services</a>
+                    <a href="/">Services</a>
                 </li>
                 <li>
-                    <a href="#">Doctors</a>
+                    <a href="/">Doctors</a>
                 </li>
                 <li>
-                    <a href="#">Products</a>
+                    <a href="/">Products</a>
                 </li>
                 <li>
-                    <a href="#">Gallary</a>
+                    <a href="/">Gallary</a>
                 </li>
             </ul>
         </div>
